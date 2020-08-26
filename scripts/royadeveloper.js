1c62c58641fd8bfc3857824b0644ff2e0cd7b988
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-08-26 21:46:54 +0100

    group task

---
+++ scripts/royadeveloper.js
@@ -0,0 +1,9 @@
+const name = "Prince Chimaobi";
+const id = "HNG-00996";
+const email = "royadeveloper01@gmail.com";
+const language = "JavaScript";
+
+const myInfo = (name, id, language) => {
+    console.log(`Hello World, This is ${name} with HNGi7 ID ${id} using ${language} for stage two task.${email}`);
+}
+myInfo(name,id,language);
